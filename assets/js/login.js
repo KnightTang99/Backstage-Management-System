460b6f6b9f964f2b1c6e9fd6327bb89c9ef131eb
itcast <1521086170@qq.com>
2020-12-31 22:06:07 +0800

    登录/注册页面ajax请求完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,4 +5,48 @@ $(function () {
   $('#link_login').on('click', function () {
     $('.login_box').show().siblings('.register_box').hide()
   })
+  const form = layui.form
+  const layer = layui.layer
+  form.verify({
+    pwd: [/^[\S]{6,12}$/, '密码必须6~12位，且不能出现空格'],
+    repwd: function (value) {
+      if (value !== $('.register_box [name=password]').val()) return '两次输入的密码不一致!'
+    }
+  })
+  $('#register_form').on('submit', function (e) {
+    e.preventDefault()
+    registerUser()
+  })
+  $('#login_form').on('submit', function (e) {
+    e.preventDefault()
+    loginUser()
+  })
 })
+function registerUser() {
+  $.ajax({
+    method: 'POST',
+    url: '/api/reguser',
+    data: {
+      username: $('.register_box [name=username]').val(),
+      password: $('.register_box [name=password]').val()
+    },
+    success(res) {
+      if (res.status !== 0) return layer.msg(res.message)
+      $('#link_login').click()
+    }
+  })
+}
+function loginUser() {
+  $.ajax({
+    method: 'POST',
+    url: '/api/login',
+    data: $('#login_form').serialize(),
+    success(res) {
+      console.log(res)
+      if (res.status !== 0) return layer.msg('登陆失败!')
+      layer.msg('登陆成功!')
+      localStorage.setItem('token', res.token)
+      location.href = '/index.html'
+    }
+  })
+}
