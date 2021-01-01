652b234b7bebd9ffcfedd574c05c3abcd8437d97
itcast <1521086170@qq.com>
2021-01-01 16:38:30 +0800

    获取用户信息并渲染

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,4 +7,27 @@ $(function () {
       layer.close(index)
     })
   })
+  getUserInfo()
 })
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    success(res) {
+      console.log(res)
+      if (res.status !== 0) return layer.msg('获取用户信息失败！')
+      renderAvatar(res.data)
+    }
+  })
+}
+function renderAvatar(user) {
+  let name = user.nickname || user.username
+  $('#welcome').html('欢迎' + name)
+  if (user.user_pic !== null) {
+    $('.layui-nav-img').attr('src', user.user_pic).show()
+    $('.text-avatar').hide()
+  } else {
+    $('.text-avatar').html(name[0].toUpperCase()).show()
+    $('.layui-nav-img').hide()
+  }
+}
