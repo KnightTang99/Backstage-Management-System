65a556b4442407a18f71cb7e8efc589a6823690f
itcast <1521086170@qq.com>
2021-01-02 16:44:05 +0800

    文章类别的相关操作

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,5 +1,6 @@
 $(function () {
   const layer = layui.layer
+  const form = layui.form
   initArtCate()
   let index = null
   $('#addBtn').on('click', function () {
@@ -17,7 +18,6 @@ $(function () {
       url: '/my/article/addcates',
       data: $('#cateForm').serialize(),
       success(res) {
-        console.log(res)
         if (res.status !== 0) return layer.msg('添加文章分类失败！')
         layer.msg('添加文章分类成功！')
         layer.close(index)
@@ -25,7 +25,62 @@ $(function () {
       }
     })
   })
+  //   修改内容
+  let editIndex = null
+  $('tbody').on('click', '.editBtn', function () {
+    editIndex = layer.open({
+      title: '修改文章分类',
+      type: 1,
+      area: ['500px', '250px'],
+      content: $('#editArt').html()
+    })
+    let id = $(this).attr('data-id')
+    $.ajax({
+      method: 'GET',
+      url: '/my/article/cates/' + id,
+      success(res) {
+        form.val('editForm', res.data)
+      }
+    })
+  })
+  $('body').on('submit', '#editForm', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/my/article/updatecate',
+      data: $(this).serialize(),
+      success(res) {
+        if (res.status !== 0) {
+          return layer.msg('更新内容失败!')
+        }
+        layer.msg('更新内容成功！')
+        layer.close(editIndex)
+        initArtCate()
+      }
+    })
+  })
+
+  //   删除功能
+  $('tbody').on('click', '.deletBtn', function () {
+    let id = $(this).attr('data-id')
+    layer.confirm('确认删除吗？', { icon: 3, title: '提示' }, function (index) {
+      //do something
+      $.ajax({
+        method: 'GET',
+        url: '/my/article/deletecate/' + id,
+        success(res) {
+          if (res.status !== 0) {
+            return layer.msg('删除分类失败！')
+          }
+          layer.msg('删除分类成功！')
+          layer.close(index)
+          initArtCate()
+        }
+      })
+    })
+  })
 })
+// 初始化函数
 function initArtCate() {
   $.ajax({
     method: 'GET',
